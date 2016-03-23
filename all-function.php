24b3bbc4fd1c6a6c9fd1301d75399eee9bc85de6
MD. SHAHAB UDDIN <support@devsit.com>
2016-03-23 11:34:48 +0600

    Update all-function.php

--- all-function.php
+++ all-function.php
@@ -4,3 +4,10 @@
 echo round(4.445543,2);
 # ceil() delete number after point 4.5555 ans:4
 echo ceil(4.445543);
+
+# ceil() delete number after point 4.5555 and add 1 with integer.Like ans:5
+echo ceil(4.445543);
+
+# floor() only delete number after point 4.5555 and do not add any thing with  integer.Like ans:4
+echo floor(4.445543);
+
